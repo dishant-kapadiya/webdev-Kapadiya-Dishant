94e328d56f61eed9ab3f0105026c6786a765ac8e
Dishant Kapadiya <kapadiya.d@husky.neu.edu>
2017-09-08 14:26:00 -0400

    resolved configuration errors for Anugular

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.set('port', port);
 // Create HTTP server
 const server = http.createServer(app);
 
-var serverSide = require("./server/test-mongodb/app");
+let serverSide = require("./server/test-mongodb/app");
 serverSide(app);
 
 
