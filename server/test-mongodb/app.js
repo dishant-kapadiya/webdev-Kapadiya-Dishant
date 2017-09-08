4cb9a2df19794bdf02367a5e9ad5b36538879160
Dishant Kapadiya <kapadiya.d@husky.neu.edu>
2017-09-07 23:57:32 -0400

    Initial version

---
+++ server/test-mongodb/app.js
@@ -0,0 +1,63 @@
+module.exports = function(app)
+{
+
+    app.get('/api', (req, res) => {
+      res.send('App works');
+    });
+    app.get("/api/test", findAllMessages);
+    app.post("/api/test", createMessage);
+    app.delete("/api/test/:id", deleteMessage);
+
+
+  let connectionString = 'mongodb://admin:admin@ds129144.mlab.com:29144/heroku_twd21hmz';
+
+  let mongoose = require("mongoose");
+    mongoose.connect(connectionString);
+
+    let TestSchema = mongoose.Schema({
+        message: String
+    });
+
+    let TestModel = mongoose.model("TestModel", TestSchema);
+
+    function findAllMessages(req, res) {
+
+        TestModel
+            .find()
+            .then(
+                function(tests) {
+                    res.json(tests);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function createMessage(req, res) {
+        console.log("in app");
+        TestModel
+            .create(req.body)
+            .then(
+                function(test) {
+                    res.json(test);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function deleteMessage(req, res) {
+        TestModel
+            .remove({_id: req.params.id})
+            .then(
+                function(result) {
+                    res.json(result);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+};
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TestComponent } from './components/test/test.component';
+import {Routing} from './app.routing';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {TestService} from './services/test.service.client';
+
+@NgModule({
+  // Declare components here
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    Routing
+  ],
+  // Client Side services here
+  providers: [ TestService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
