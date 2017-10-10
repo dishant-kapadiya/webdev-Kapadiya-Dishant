f5e2a62b205a756001443d7ffa35d9cdddb139cd
Dishant Kapadiya <kapadiya.d@husky.neu.edu>
2017-10-09 20:06:01 -0400

    login routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,55 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {Routing} from './app.routing';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
-import {TestService} from './services/test.service.client';
-import { LoginComponent } from './components/user/login/login.component';
-import { ProfileComponent } from './components/user/profile/profile.component';
-import { RegisterComponent } from './components/user/register/register.component';
-import { WebsiteNewComponent } from './components/website/website-new/website-new.component';
-import { WebsiteEditComponent } from './components/website/website-edit/website-edit.component';
-import { WebsiteListComponent } from './components/website/website-list/website-list.component';
-import { PageNewComponent } from './components/page/page-new/page-new.component';
-import { PageEditComponent } from './components/page/page-edit/page-edit.component';
-import { PageListComponent } from './components/page/page-list/page-list.component';
-import { WidgetChooseComponent } from './components/widget/widget-choose/widget-choose.component';
-import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
-import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
-import { WidgetHeaderComponent } from './components/widget/widget-edit/widget-header/widget-header.component';
-import { WidgetImageComponent } from './components/widget/widget-edit/widget-image/widget-image.component';
-import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
+import {LoginComponent} from './components/user/login/login.component';
+import {ProfileComponent} from './components/user/profile/profile.component';
+import {RegisterComponent} from './components/user/register/register.component';
+import {WebsiteNewComponent} from './components/website/website-new/website-new.component';
+import {WebsiteEditComponent} from './components/website/website-edit/website-edit.component';
+import {WebsiteListComponent} from './components/website/website-list/website-list.component';
+import {PageNewComponent} from './components/page/page-new/page-new.component';
+import {PageEditComponent} from './components/page/page-edit/page-edit.component';
+import {PageListComponent} from './components/page/page-list/page-list.component';
+import {WidgetChooseComponent} from './components/widget/widget-choose/widget-choose.component';
+import {WidgetEditComponent} from './components/widget/widget-edit/widget-edit.component';
+import {WidgetListComponent} from './components/widget/widget-list/widget-list.component';
+import {WidgetHeaderComponent} from './components/widget/widget-edit/widget-header/widget-header.component';
+import {WidgetImageComponent} from './components/widget/widget-edit/widget-image/widget-image.component';
+import {WidgetYoutubeComponent} from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
+import {UserService} from './services/user.service.client';
+import {WebsiteService} from './services/website.service.client';
+import {PageService} from './services/page.service.client';
+import {WidgetService} from './services/widget.service.client';
 
 @NgModule({
-  // Declare components here
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ProfileComponent,
-    RegisterComponent,
-    WebsiteNewComponent,
-    WebsiteEditComponent,
-    WebsiteListComponent,
-    PageNewComponent,
-    PageEditComponent,
-    PageListComponent,
-    WidgetChooseComponent,
-    WidgetEditComponent,
-    WidgetListComponent,
-    WidgetHeaderComponent,
-    WidgetImageComponent,
-    WidgetYoutubeComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    FormsModule,
-    Routing
-  ],
-  // Client Side services here
-  providers: [ TestService],
-  bootstrap: [AppComponent]
+    // Declare components here
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        ProfileComponent,
+        RegisterComponent,
+        WebsiteNewComponent,
+        WebsiteEditComponent,
+        WebsiteListComponent,
+        PageNewComponent,
+        PageEditComponent,
+        PageListComponent,
+        WidgetChooseComponent,
+        WidgetEditComponent,
+        WidgetListComponent,
+        WidgetHeaderComponent,
+        WidgetImageComponent,
+        WidgetYoutubeComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        Routing
+    ],
+    // Client Side services here
+    providers: [UserService, WebsiteService, PageService, WidgetService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {UserService} from '../../../services/user.service.client';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('f') loginForm: NgForm;
 
-  ngOnInit() {
-  }
+    username: string;
+    password: string;
+    errorFlag: boolean;
+    errorMsg: 'Invalid username or password';
 
+    constructor(private router: Router, private serviceHandler: UserService) {
+    }
+
+    ngOnInit() {
+    }
+
+    login() {
+        this.username = this.loginForm.value.username;
+        this.password = this.loginForm.value.password;
+        const user = this.serviceHandler.findUserByCredentials(this.username, this.password);
+        if (user) {
+            this.errorFlag = false;
+            const uid = user._id;
+            this.router.navigate(['user', uid]);
+        } else {
+            this.errorFlag = true;
+        }
+    }
 }
