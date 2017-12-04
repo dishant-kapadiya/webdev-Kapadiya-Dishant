4117bb0725317c541627faa51c2087c246236c73
Dishant Kapadiya <kapadiya.d@husky.neu.edu>
2017-12-04 00:06:50 -0500

    modified password encryption and login

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // CORS
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
